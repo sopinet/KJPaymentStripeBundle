5cd3c5c4f707b071de29fae6054f2074bc554770
Keng Jin Chew <kjchew@gmail.com>
2013-10-16 10:48:38 +0100

    disable rendering of optional text on card form

--- Form/StripeCreditCardType.php
+++ Form/StripeCreditCardType.php
@@ -18,78 +18,89 @@ class StripeCreditCardType extends AbstractType
         $builder
             ->add('name', 'text', array(
                 'label' => 'Your name',
-                'required' => false,
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('number', 'text', array(
                 'label' => 'Card number',
-                'required' => false,
                 'error_type' => 'block',
                 'attr' => array(
                     'maxlength' => 19,
                 ),
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('exp_month', 'choice', array(
-                'label' => 'Card Expiry',
-                'required' => false,
+                'label' => 'Card expiry',
                 'error_type' => 'block',
                 'choices' => array('01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May', '06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec'),
                 'empty_value' => 'MM',
                 'attr' => array(
                     'class' => 'input-mini',
                 ),
-            ))
-            ->add('exp_year', 'choice', array(
-                'label' => ' ',
                 'required' => false,
+                'render_optional_text' => false,
+            ))
+            ->add('exp_year', 'optionalchoice', array(
+                'label' => ' ',
                 'error_type' => 'block',
                 'empty_value' => 'YYYY',
                 'choices' => array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20)),
                 'attr' => array(
                     'class' => 'input-small',
                 ),
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('cvc', 'text', array(
                 'label' => 'CVC',
-                'required' => false,
                 'error_type' => 'block',
                 'attr' => array(
                     'class' => 'input-mini',
                     'maxlength' => 4,
                 ),
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_line1', 'text', array(
-                'label' => 'Billing Address',
-                'required' => false,
+                'label' => 'Billing address',
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_line2', 'text', array(
-                'label' => false,
                 'required' => false,
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_city', 'text', array(
                 'label' => 'City',
-                'required' => false,
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_state', 'text', array(
                 'label' => 'State',
-                'required' => false,
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_country', 'text', array(
                 'label' => 'Country',
-                'required' => false,
                 'error_type' => 'block',
+                'required' => false,
+                'render_optional_text' => false,
             ))
             ->add('address_zip', 'text', array(
                 'label' => 'Postcode / Zip code',
-                'required' => false,
                 'error_type' => 'block',
                 'attr' => array(
                     'class' => 'input-small',
                 ),
+                'required' => false,
+                'render_optional_text' => false,
             ));
     }
 }
