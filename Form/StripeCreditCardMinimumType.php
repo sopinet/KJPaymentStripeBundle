b06d017183afc1d1a00ef45b981af816f0b8ad76
Fernando Hidalgo <fernanhidabe@hotmail.com>
2019-04-05 20:59:08 +0200

    fixed sf34

--- Form/StripeCreditCardMinimumType.php
+++ Form/StripeCreditCardMinimumType.php
@@ -30,7 +30,7 @@ class StripeCreditCardMinimumType extends AbstractType
             ->add('exp_month', 'choice', array(
                 'label' => 'Card expiry',
                 'choices' => array('01' => '01', '02' => '02', '03' => '03', '04' => '04', '05' => '05', '06' => '06', '07' => '07', '08' => '08', '09' => '09', '10' => '10', '11' => '11', '12' => '12'),
-                'empty_value' => 'MM',
+                'empty_data' => 'MM',
                 'attr' => array(
                     'class' => 'input-mini',
                 ),
@@ -38,7 +38,7 @@ class StripeCreditCardMinimumType extends AbstractType
             ))
             ->add('exp_year', 'choice', array(
                 'label' => ' ',
-                'empty_value' => 'YYYY',
+                'empty_data' => 'YYYY',
                 'choices' => array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20)),
                 'attr' => array(
                     'class' => 'input-small',
