f770c3f25876c8cbb68cc4667a1805984cdbf898
Fernando Hidalgo <fernanhidabe@hotmail.com>
2019-04-06 18:40:57 +0200

    fixed default

--- Form/StripeCreditCardMinimumType.php
+++ Form/StripeCreditCardMinimumType.php
@@ -29,7 +29,7 @@ class StripeCreditCardMinimumType extends AbstractType
             ))
             ->add('exp_month', 'choice', array(
                 'label' => 'Card expiry',
-                'choices' => array('01' => '01', '02' => '02', '03' => '03', '04' => '04', '05' => '05', '06' => '06', '07' => '07', '08' => '08', '09' => '09', '10' => '10', '11' => '11', '12' => '12'),
+                'choices' => array('MM' => 'MM', '01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May', '06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec'),
                 'empty_data' => 'MM',
                 'attr' => array(
                     'class' => 'input-mini',
@@ -39,7 +39,7 @@ class StripeCreditCardMinimumType extends AbstractType
             ->add('exp_year', 'choice', array(
                 'label' => ' ',
                 'empty_data' => 'YYYY',
-                'choices' => array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20)),
+                'choices' => array_merge(array('YYYY'), array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20))),
                 'attr' => array(
                     'class' => 'input-small',
                 ),
