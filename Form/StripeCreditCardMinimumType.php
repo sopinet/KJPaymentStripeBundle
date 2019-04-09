087f86f6dad834fca9124f11b48911a5fbf7b73d
alejandro <aarrabal@sopinet.com>
2019-04-09 09:08:18 +0200

    Added dependencies

--- Form/StripeCreditCardMinimumType.php
+++ Form/StripeCreditCardMinimumType.php
@@ -4,7 +4,8 @@ namespace KJ\Payment\StripeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StripeCreditCardMinimumType extends AbstractType
 {
@@ -16,18 +17,18 @@ class StripeCreditCardMinimumType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', 'text', array(
+            ->add('name', TextType::class, array(
                 'label' => 'Your name',
                 'required' => false,
             ))
-            ->add('number', 'text', array(
+            ->add('number', TextType::class, array(
                 'label' => 'Card number',
                 'attr' => array(
                     'maxlength' => 19,
                 ),
                 'required' => false,
             ))
-            ->add('exp_month', 'choice', array(
+            ->add('exp_month', ChoiceType::class, array(
                 'label' => 'Card expiry',
                 'choices' => array('MM' => 'MM', '01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May', '06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec'),
                 'empty_data' => 'MM',
@@ -36,16 +37,16 @@ class StripeCreditCardMinimumType extends AbstractType
                 ),
                 'required' => false,
             ))
-            ->add('exp_year', 'choice', array(
+            ->add('exp_year', ChoiceType::class, array(
                 'label' => ' ',
                 'empty_data' => 'YYYY',
-                'choices' => array_merge(array('YYYY'), array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20))),
+                'choices' => array_merge(array('YYYY'), array_combine(range(date('Y'), date('Y') + 40), range(date('Y'), date('Y') + 40))),
                 'attr' => array(
                     'class' => 'input-small',
                 ),
                 'required' => false,
             ))
-            ->add('cvc', 'text', array(
+            ->add('cvc', TextType::class, array(
                 'label' => 'CVC',
                 'attr' => array(
                     'class' => 'input-mini',
--- Form/StripeCreditCardType.php
+++ Form/StripeCreditCardType.php
@@ -4,7 +4,8 @@ namespace KJ\Payment\StripeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StripeCreditCardType extends AbstractType
 {
@@ -16,18 +17,18 @@ class StripeCreditCardType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', 'text', array(
+            ->add('name', TextType::class, array(
                 'label' => 'Your name',
                 'required' => false,
             ))
-            ->add('number', 'text', array(
+            ->add('number', TextType::class, array(
                 'label' => 'Card number',
                 'attr' => array(
                     'maxlength' => 19,
                 ),
                 'required' => false,
             ))
-            ->add('exp_month', 'choice', array(
+            ->add('exp_month', ChoiceType::class, array(
                 'label' => 'Card expiry',
                 'choices' => array('MM' => 'MM', '01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May', '06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec'),
                 'empty_data' => 'MM',
@@ -36,10 +37,10 @@ class StripeCreditCardType extends AbstractType
                 ),
                 'required' => false,
             ))
-            ->add('exp_year', 'choice', array(
+            ->add('exp_year', ChoiceType::class, array(
                 'label' => ' ',
                 'empty_data' => 'YYYY',
-                'choices' => array_merge(array('YYYY'), array_combine(range(date('Y'), date('Y') + 20), range(date('Y'), date('Y') + 20))),
+                'choices' => array_merge(array('YYYY'), array_combine(range(date('Y'), date('Y') + 40), range(date('Y'), date('Y') + 40))),
                 'attr' => array(
                     'class' => 'input-small',
                 ),
@@ -53,26 +54,26 @@ class StripeCreditCardType extends AbstractType
                 ),
                 'required' => false,
             ))
-            ->add('address_line1', 'text', array(
+            ->add('address_line1', TextType::class, array(
                 'label' => 'Billing address',
                 'required' => false,
             ))
-            ->add('address_line2', 'text', array(
+            ->add('address_line2', TextType::class, array(
                 'required' => false,
             ))
-            ->add('address_city', 'text', array(
+            ->add('address_city', TextType::class, array(
                 'label' => 'City',
                 'required' => false,
             ))
-            ->add('address_state', 'text', array(
+            ->add('address_state', TextType::class, array(
                 'label' => 'State',
                 'required' => false,
             ))
-            ->add('address_country', 'text', array(
+            ->add('address_country', TextType::class, array(
                 'label' => 'Country',
                 'required' => false,
             ))
-            ->add('address_zip', 'text', array(
+            ->add('address_zip', TextType::class, array(
                 'label' => 'Postcode / Zip code',
                 'attr' => array(
                     'class' => 'input-small',
